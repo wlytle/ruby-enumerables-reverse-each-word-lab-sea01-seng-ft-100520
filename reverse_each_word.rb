edd0f9865b6f0e90c0c3db9065477e95e457d199
Will Lytle <wlytle89@gmail.com>
2020-08-15 21:17:03 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,9 @@
 def reverse_each_word(string)
   string_array = string.split(" ")
-  reverse_array = []
-  string_array.each do |word|
+  
+  reverse_array = string_array.collect do |word|
     word_array = word.split("")
-    reverse_array << word_array.reverse.join
+    word_array.reverse.join
   end
   reverse_array.join(" ")
 end
